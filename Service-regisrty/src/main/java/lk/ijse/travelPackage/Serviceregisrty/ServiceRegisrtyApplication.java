088cc821f930239b9732910e2ac22472efd8b3de
oshada1999 <chathurahewage1999@gmail.com>
2023-11-03 14:20:29 +0530

    add service registry

---
+++ Service-regisrty/src/main/java/lk/ijse/travelPackage/Serviceregisrty/ServiceRegisrtyApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.travelPackage.Serviceregisrty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegisrtyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegisrtyApplication.class, args);
+	}
+
+}
